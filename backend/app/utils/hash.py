9f5902a1e115b2e0a1a50acd6e18ca34930cf3a4
Raja18110 <rkumari05195rr@gmail.com>
2026-03-25 21:29:55 +0530

    Add password hashing using bcrypt

---
+++ backend/app/utils/hash.py
@@ -0,0 +1,7 @@
+# app/utils/hash.py
+from passlib.context import CryptContext
+
+pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(p): return pwd.hash(p)
+def verify_password(p, h): return pwd.verify(p, h)
