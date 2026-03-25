b98940c1136d6066516aa22d91740f4e53f1c055
Raja18110 <rkumari05195rr@gmail.com>
2026-03-25 19:29:01 +0530

    add database connection with neon postgresql

---
+++ backend/app/db.py
@@ -0,0 +1,8 @@
+# app/db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+engine = create_engine(os.getenv("DATABASE_URL"))
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
